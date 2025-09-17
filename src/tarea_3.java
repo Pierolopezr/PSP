c1d0d1668ee3a63fd85f2c305c84993cef14035b
piero <plopezr@danielcastelao.org>
2025-09-17 09:22:18 +0200

    tarea3 -  terminada

--- src/tarea_3.java
+++ src/tarea_3.java
@@ -9,17 +9,8 @@ public class tarea_3 {
     public static void main(String[] args) throws IOException {
         Scanner op = new Scanner(System.in);
         System.out.println("¿Cuál es el nombre de tu archivo a abrir?");
-        String archivo = op.nextLine();
-        String notas = "prueba.txt";
+        String notas = op.nextLine();
         ProcessBuilder pBloc = new ProcessBuilder("gnome-text-editor", notas);
-        Process p;
-        if (archivo == notas){
-            pBloc.start();
-        }
-        else {
-            System.out.println("Archivo no encontrado");
-        }
+        Process p = pBloc.start();
     }
 }
-// Scanner op = New Scanner(System.in);
-//
