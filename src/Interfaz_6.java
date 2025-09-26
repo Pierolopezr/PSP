f0a0e292b6e4f7c50e8345df704a21f1cac9e849
Piero <plopezrosas@danielcastelao.org>
2025-09-26 21:43:27 +0200

    tarea 6

---
+++ src/Interfaz_6.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+/**
+ * tarea 6 main
+ * @author Piero López Rosas
+ */
+public class Interfaz_6 {
+
+        public static void main(String[] args) {
+            Scanner op = new Scanner(System.in);
+            String entrada;
+
+            // Bucle infinito que solo se romperá cuando el usuario escriba "salir"
+            while (true) {
+                System.out.println("Introduce el host o IP o 'salir' para finalizar:");
+                entrada = op.nextLine();
+
+                if (entrada.equalsIgnoreCase("salir")) {
+                    System.out.println("Saliendo del programa...");
+                    break;
+                } else {
+                    Lanzador_6.ejecutarPing(entrada); // llamamos a Lanzador
+                }
+            }
+
+        }
+
+}
---
+++ src/Lanzador_6.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+/**
+ * tarea 6 ejecutar ping
+ * @author Piero López Rosas
+ */
+public class Lanzador_6 {
+        public static void ejecutarPing(String host) {
+            //host recibe el valor de entrada
+            String os = System.getProperty("os.name").toLowerCase();
+            ProcessBuilder pb;
+            // El comando ping es diferente en Windows y en Linux/Mac : https://axarnet.es/blog/ping-ip-windows-macos-linux
+            if (os.contains("windows")) {
+                // Estamos en Windows
+                pb = new ProcessBuilder("ping", "-n", "4", host);
+            } else {
+                // Estamos en Linux, Mac, etc.
+                pb = new ProcessBuilder("ping", "-c", "4", host);
+            }
+
+            try {
+
+                pb.inheritIO(); // Hago la salida del proceso ping se muestre en nuestra consola de Java en vez de una terminal.
+                Process proceso = pb.start();
+
+                int codigoSalida = proceso.waitFor();  //espero aquí hasta que el proceso "ping" termine.
+
+                System.out.println("Operación completada. Código de salida: " + codigoSalida); // Muestra el código de salida que nos ha devuelto el proceso.
+
+            } catch (IOException | InterruptedException e) {
+                // uso de los dos tipos de Exception
+                // InterruptedException :  https://programmerclick.com/article/8830618963/
+                // IOException : https://programmerclick.com/article/8830618963/
+                System.err.println("Error al ejecutar el comando: " + e.getMessage());
+            }
+        }
+
+}
