0c4845df5ffd946a36f924b31f197e40fb2d932e
Piero <plopezrosas@danielcastelao.org>
2025-10-03 23:37:45 +0200

    randomm

--- src/random.java
+++ src/random.java
@@ -2,16 +2,23 @@ import java.io.IOException;
 
 public class random {
     public static void main(String[] args) throws InterruptedException {
-        ProcessBuilder pb = new ProcessBuilder("sleep", "5");
-        Process p = null;
+        String os = System.getProperty("os.name").toLowerCase();
+        ProcessBuilder pb;
 
+        if (os.contains("win")) {
+            pb = new ProcessBuilder("cmd", "/c", "timeout", "/T", "5");
+        } else {
+            pb = new ProcessBuilder("sleep", "5");
+        }
+
+        Process p = null;
         try {
             p = pb.start();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        // Bucle sin bloquear: hacemos otra cosas mientras el hijo vive
+        // Bucle sin bloquear: hacemos otra cosa mientras el hijo vive
         while (p.isAlive()) {
             System.out.println("El hijo vivo...");
             Thread.sleep(1000); //simulamos otras tareas
