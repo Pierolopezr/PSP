0f636e7ba7d10acb9bb90a5811a6793b53bba621
piero <plopezr@danielcastelao.org>
2026-01-13 10:06:39 +0100

    Esqueleto 2

--- src/tarea_38/tarea_38.java
+++ src/tarea_38/tarea_38.java
@@ -3,9 +3,11 @@ package tarea_38;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HexFormat;
+import java.util.Scanner;
 
 public class tarea_38 {
     public static void main(String[] args) throws NoSuchAlgorithmException {
+        /*
         // Instanciar
         MessageDigest md = MessageDigest.getInstance("SHA-256");
 
@@ -20,8 +22,41 @@ public class tarea_38 {
 
         // COnvertir a Hexadecimal (Legible)
         String hex = HexFormat.of().formatHex(resumen);
-        System.out.println(hex); // Imprime: a5f3
+        System.out.println(hex); // Imprime: a5f3...
+        */
 
+
+
+
+
+        // FASE DE REGISTRO
+        String contrasena = "";
+        System.out.println("Hola, bienvenido. Crea una contraseña: ");
+        Scanner op = new Scanner(System.in);
+        contrasena = op.nextLine();
+
+        // Instanciar
+        MessageDigest mp = MessageDigest.getInstance("SHA-256");
+        // Cargar datos
+        mp.update(contrasena.getBytes());
+
+        // Resumir (Devuelve bytes)
+        byte[] resumen2 = mp.digest();
+
+
+        // COnvertir a Hexadecimal (Legible)
+        String hex2 = HexFormat.of().formatHex(resumen2);
+        System.out.println(hex2);
+
+        // FASE DE LOGIN
+        System.out.println("Usuario registrado. Inicie sesión para probar: ");
+        String contrasena2 = op.nextLine();
+
+        if (contrasena2.equals(contrasena)){
+            System.out.println("Acceso concedido");
+        }else {
+            System.out.println("ERROR: Credenciales inválidas");
+        }
 
     }
 }
