4e6a255c915e8ae860dc2a87e05fa111bee2f898
piero <plopezr@danielcastelao.org>
2026-01-21 11:52:58 +0100

    Esqueleto

--- src/tarea_32/Cliente.java
+++ src/tarea_32/Cliente.java
@@ -1,5 +1,19 @@
 package tarea_32;
 
-public class Cliente {
+import java.io.IOException;
+import java.net.SocketException;
 
+public class Cliente {
+    public static void main(String[] args) {
+        int puerto_servidor = 8888;
+        byte[] buffer = new byte[1024];
+
+        try{
+
+        }catch (SocketException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
